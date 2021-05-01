d1b0a7faa876a6597021331d186fed2f1164844d
Никита Митраченков <nickitaipad@gmail.com>
2021-05-01 16:51:47 +0300

    add

---
+++ work1/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace work1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNum1 = new System.Windows.Forms.Label();
+            this.labelNum2 = new System.Windows.Forms.Label();
+            this.labelNum3 = new System.Windows.Forms.Label();
+            this.labelNum4 = new System.Windows.Forms.Label();
+            this.labelNum5 = new System.Windows.Forms.Label();
+            this.labelNum6 = new System.Windows.Forms.Label();
+            this.labelNum7 = new System.Windows.Forms.Label();
+            this.labelNum8 = new System.Windows.Forms.Label();
+            this.labelNum9 = new System.Windows.Forms.Label();
+            this.labelNum10 = new System.Windows.Forms.Label();
+            this.labelNum11 = new System.Windows.Forms.Label();
+            this.labelNum12 = new System.Windows.Forms.Label();
+            this.labelNum13 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelNum1
+            // 
+            this.labelNum1.AutoSize = true;
+            this.labelNum1.Location = new System.Drawing.Point(79, 86);
+            this.labelNum1.Name = "labelNum1";
+            this.labelNum1.Size = new System.Drawing.Size(35, 13);
+            this.labelNum1.TabIndex = 0;
+            this.labelNum1.Text = "label1";
+            // 
+            // labelNum2
+            // 
+            this.labelNum2.AutoSize = true;
+            this.labelNum2.Location = new System.Drawing.Point(132, 86);
+            this.labelNum2.Name = "labelNum2";
+            this.labelNum2.Size = new System.Drawing.Size(35, 13);
+            this.labelNum2.TabIndex = 1;
+            this.labelNum2.Text = "label2";
+            // 
+            // labelNum3
+            // 
+            this.labelNum3.AutoSize = true;
+            this.labelNum3.Location = new System.Drawing.Point(184, 86);
+            this.labelNum3.Name = "labelNum3";
+            this.labelNum3.Size = new System.Drawing.Size(35, 13);
+            this.labelNum3.TabIndex = 2;
+            this.labelNum3.Text = "label3";
+            // 
+            // labelNum4
+            // 
+            this.labelNum4.AutoSize = true;
+            this.labelNum4.Location = new System.Drawing.Point(235, 86);
+            this.labelNum4.Name = "labelNum4";
+            this.labelNum4.Size = new System.Drawing.Size(35, 13);
+            this.labelNum4.TabIndex = 3;
+            this.labelNum4.Text = "label4";
+            // 
+            // labelNum5
+            // 
+            this.labelNum5.AutoSize = true;
+            this.labelNum5.Location = new System.Drawing.Point(277, 86);
+            this.labelNum5.Name = "labelNum5";
+            this.labelNum5.Size = new System.Drawing.Size(35, 13);
+            this.labelNum5.TabIndex = 4;
+            this.labelNum5.Text = "label5";
+            // 
+            // labelNum6
+            // 
+            this.labelNum6.AutoSize = true;
+            this.labelNum6.Location = new System.Drawing.Point(319, 86);
+            this.labelNum6.Name = "labelNum6";
+            this.labelNum6.Size = new System.Drawing.Size(35, 13);
+            this.labelNum6.TabIndex = 5;
+            this.labelNum6.Text = "label6";
+            // 
+            // labelNum7
+            // 
+            this.labelNum7.AutoSize = true;
+            this.labelNum7.Location = new System.Drawing.Point(361, 86);
+            this.labelNum7.Name = "labelNum7";
+            this.labelNum7.Size = new System.Drawing.Size(35, 13);
+            this.labelNum7.TabIndex = 6;
+            this.labelNum7.Text = "label7";
+            // 
+            // labelNum8
+            // 
+            this.labelNum8.AutoSize = true;
+            this.labelNum8.Location = new System.Drawing.Point(403, 86);
+            this.labelNum8.Name = "labelNum8";
+            this.labelNum8.Size = new System.Drawing.Size(35, 13);
+            this.labelNum8.TabIndex = 7;
+            this.labelNum8.Text = "label8";
+            // 
+            // labelNum9
+            // 
+            this.labelNum9.AutoSize = true;
+            this.labelNum9.Location = new System.Drawing.Point(445, 86);
+            this.labelNum9.Name = "labelNum9";
+            this.labelNum9.Size = new System.Drawing.Size(35, 13);
+            this.labelNum9.TabIndex = 8;
+            this.labelNum9.Text = "label9";
+            // 
+            // labelNum10
+            // 
+            this.labelNum10.AutoSize = true;
+            this.labelNum10.Location = new System.Drawing.Point(498, 86);
+            this.labelNum10.Name = "labelNum10";
+            this.labelNum10.Size = new System.Drawing.Size(41, 13);
+            this.labelNum10.TabIndex = 9;
+            this.labelNum10.Text = "label10";
+            // 
+            // labelNum11
+            // 
+            this.labelNum11.AutoSize = true;
+            this.labelNum11.Location = new System.Drawing.Point(187, 285);
+            this.labelNum11.Name = "labelNum11";
+            this.labelNum11.Size = new System.Drawing.Size(41, 13);
+            this.labelNum11.TabIndex = 10;
+            this.labelNum11.Text = "label11";
+            // 
+            // labelNum12
+            // 
+            this.labelNum12.AutoSize = true;
+            this.labelNum12.Location = new System.Drawing.Point(270, 285);
+            this.labelNum12.Name = "labelNum12";
+            this.labelNum12.Size = new System.Drawing.Size(41, 13);
+            this.labelNum12.TabIndex = 11;
+            this.labelNum12.Text = "label12";
+            // 
+            // labelNum13
+            // 
+            this.labelNum13.AutoSize = true;
+            this.labelNum13.Location = new System.Drawing.Point(355, 285);
+            this.labelNum13.Name = "labelNum13";
+            this.labelNum13.Size = new System.Drawing.Size(41, 13);
+            this.labelNum13.TabIndex = 12;
+            this.labelNum13.Text = "label13";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(273, 186);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 13;
+            this.buttonStart.Text = "старт";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelNum13);
+            this.Controls.Add(this.labelNum12);
+            this.Controls.Add(this.labelNum11);
+            this.Controls.Add(this.labelNum10);
+            this.Controls.Add(this.labelNum9);
+            this.Controls.Add(this.labelNum8);
+            this.Controls.Add(this.labelNum7);
+            this.Controls.Add(this.labelNum6);
+            this.Controls.Add(this.labelNum5);
+            this.Controls.Add(this.labelNum4);
+            this.Controls.Add(this.labelNum3);
+            this.Controls.Add(this.labelNum2);
+            this.Controls.Add(this.labelNum1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNum1;
+        private System.Windows.Forms.Label labelNum2;
+        private System.Windows.Forms.Label labelNum3;
+        private System.Windows.Forms.Label labelNum4;
+        private System.Windows.Forms.Label labelNum5;
+        private System.Windows.Forms.Label labelNum6;
+        private System.Windows.Forms.Label labelNum7;
+        private System.Windows.Forms.Label labelNum8;
+        private System.Windows.Forms.Label labelNum9;
+        private System.Windows.Forms.Label labelNum10;
+        private System.Windows.Forms.Label labelNum11;
+        private System.Windows.Forms.Label labelNum12;
+        private System.Windows.Forms.Label labelNum13;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ work1/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace work1
+{
+    public partial class Form1 : Form
+    {
+        int numbers2 = 0;
+        int numbers3 = 0;
+        int result = 0;
+        int numberNeZnayu = 0;
+        int numberNeZnayu2 = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int number1 = random.Next(1, 3 + 1);
+            int number2 = random.Next(1, 3 + 1);
+            int number3 = random.Next(1, 3 + 1);
+            int number4 = random.Next(1, 3 + 1);
+            int number5 = random.Next(1, 3 + 1);
+            int number6 = random.Next(1, 3 + 1);
+            int number7 = random.Next(1, 3 + 1);
+            int number8 = random.Next(1, 3 + 1);
+            int number9 = random.Next(1, 3 + 1);
+            int number10 = random.Next(1, 3 + 1);
+            labelNum1.Text = number1.ToString();
+            labelNum2.Text = number2.ToString();
+            labelNum3.Text = number2.ToString();
+            labelNum4.Text = number4.ToString();
+            labelNum5.Text = number5.ToString();
+            labelNum6.Text = number6.ToString();
+            labelNum7.Text = number7.ToString();
+            labelNum8.Text = number8.ToString();
+            labelNum9.Text = number9.ToString();
+            labelNum10.Text = number10.ToString();
+
+            if (number1 == 2)
+            {
+                numbers2++;
+            }
+            else
+            {
+                if (number1 == 3)
+                {
+                    numbers3++;
+                }
+            }
+            if (number2 == 2)
+            {
+                numbers2++;
+            }
+            else
+            {
+                if (number2 == 3)
+                {
+                    numbers3++;
+                }
+            }
+            if (number3 == 2)
+            {
+                numbers2++;
+            }
+            else
+            {
+                if (number3 == 3)
+                {
+                    numbers3++;
+                }
+            }
+            if (number4 == 2)
+            {
+                numbers2++;
+            }
+            else
+            {
+                if (number4 == 3)
+                {
+                    numbers3++;
+                }
+            }
+            if (number5 == 2)
+            {
+                numbers2++;
+            }
+            else
+            {
+                if (number5 == 3)
+                {
+                    numbers3++;
+                }
+            }
+            if (number6 == 2)
+            {
+                numbers2++;
+            }
+            else
+            {
+                if (number6 == 3)
+                {
+                    numbers3++;
+                }
+            }
+            if (number7 == 2)
+            {
+                numbers2++;
+            }
+            else
+            {
+                if (number7 == 3)
+                {
+                    numbers3++;
+                }
+            }
+            if (number8 == 2)
+            {
+                numbers2++;
+            }
+            else
+            {
+                if (number8 == 3)
+                {
+                    numbers3++;
+                }
+            }
+            if (number9 == 2)
+            {
+                numbers2++;
+            }
+            else
+            {
+                if (number9 == 3)
+                {
+                    numbers3++;
+                }
+            }
+            if (number10 == 2)
+            {
+                numbers2++;
+            }
+            else
+            {
+                if (number10 == 3)
+                {
+                    numbers3++;
+                }
+            }
+            result = numbers2 - numbers3;
+            labelNum11.Text = result.ToString();
+            if (number2 == 3)
+            {
+                numberNeZnayu++;
+            }
+            if (number4 == 3)
+            {
+                numberNeZnayu++;
+            }
+            if (number6 == 3)
+            {
+                numberNeZnayu++;
+            }
+            if (number8 == 3)
+            {
+                numberNeZnayu++;
+            }
+            if (number10 == 3)
+            {
+                numberNeZnayu++;
+            }
+            labelNum12.Text = numberNeZnayu.ToString();
+
+            if (number1 == 2)
+            {
+                numberNeZnayu2++;
+            }
+            if (number2 == 2)
+            {
+                numberNeZnayu2++;
+            }
+            if (number3 == 2)
+            {
+                numberNeZnayu2++;
+            }
+            if (number4 == 2)
+            {
+                numberNeZnayu2++;
+            }
+            if (number5 == 2)
+            {
+                numberNeZnayu2++;
+            }
+            labelNum13.Text = numberNeZnayu2.ToString();
+        }
+    }
+}
